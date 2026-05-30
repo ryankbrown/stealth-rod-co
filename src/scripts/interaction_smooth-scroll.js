7402d147e6a4bd8143945c50f824c06684b6bfd5
ryankbrown <ryankbrown@users.noreply.github.com>
2026-05-30 10:09:57 -0500

    stoppping water animation to see if it improves safari

--- src/scripts/interaction_smooth-scroll.js
+++ src/scripts/interaction_smooth-scroll.js
@@ -11,7 +11,9 @@ export default class Interaction_SiteSmoothScroll {
 		this.app = app;
 		this.opts = opts;
 		this.app.gsap_ctx.add(()=> {
+			ScrollTrigger.normalizeScroll(true);
 			this.smooth_scroller = ScrollSmoother.create(opts, gsap); 	
+			
 		})
 	}
 	
