8bf0bae1c4b6f5619380ead1500e0f73a6cd5aba
ryankbrown <ryankbrown@users.noreply.github.com>
2026-06-01 08:51:52 -0500

    test

--- src/scripts/interaction_smooth-scroll.js
+++ src/scripts/interaction_smooth-scroll.js
@@ -10,9 +10,8 @@ export default class Interaction_SiteSmoothScroll {
 		this.app = app;
 		this.opts = opts;
 		this.app.gsap_ctx.add(()=> {
-			// ScrollTrigger.normalizeScroll(true);
 			
-			// ScrollTrigger.normalizeScroll(true);
+			ScrollTrigger.normalizeScroll(true); 
 			// ScrollTrigger.config({ ignoreMobileResize: true });
 			
 			this.smooth_scroller = ScrollSmoother.create(opts, gsap); 	
