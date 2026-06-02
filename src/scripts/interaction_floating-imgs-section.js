7720520a095273136f4e04cbd3c400c5d6de6b9d
ryankbrown <ryankbrown@users.noreply.github.com>
2026-06-02 17:08:07 -0500

    test

--- src/scripts/interaction_floating-imgs-section.js
+++ src/scripts/interaction_floating-imgs-section.js
@@ -39,39 +39,39 @@ export default class Interaction_FloatingImgSection {
 
 	createMainOverlayTimeline() {
 		
-		const paragraph = new SplitText(
-			this.floating_section_el.querySelector(".about__intro-paragraph"), 
-			{ type: "lines"}
-		);
+		// const paragraph = new SplitText(
+		// 	this.floating_section_el.querySelector(".about__intro-paragraph"), 
+		// 	{ type: "lines"}
+		// );
 		
 		this.main_scroll_tl = gsap.timeline({paused: true})
 
 		// - - - - - TWEENS - - - - -				
 		.addLabel('scroll-started')
 		
-		.from(this.intro_text_content, { duration: .25 }) // wait
-		.from(paragraph.lines, {
-			autoAlpha: 0,
-			duration: 1.25,
-			stagger: 0.15,
-			ease: "power1.out"
-		}, '<')
-		// .from(".about__intro-paragraph", {
+		.from(this.floating_section_el, { duration: .25 }) // wait
+		// .from(paragraph.lines, {
 		// 	autoAlpha: 0,
 		// 	duration: 1.25,
+		// 	stagger: 0.15,
 		// 	ease: "power1.out"
-		// })
+		// }, '<')
+		.from(".about__intro-paragraph", {
+			autoAlpha: 0,
+			duration: 1.25,
+			ease: "power1.out"
+		})
 		
 		// - - - - - TWEEN MIDPOINT - - - - -	
 		.addLabel('scroll-midpoint')
-		.from(this.intro_text_content, { duration: 8 }) // wait
-		.to('.about__intro-text-content', {
-			yPercent: -10,
-			alpha: 0,
-			duration: 3,
-			ease: "power4.in"
-		})
-		.from(this.intro_text_content, { duration: .25 })
+		.from(this.floating_section_el, { duration: 8 }) // wait
+		// .to('.about__intro-text-content', {
+		// 	yPercent: -10,
+		// 	alpha: 0,
+		// 	duration: 3,
+		// 	ease: "power4.in"
+		// })
+		.from(this.floating_section_el, { duration: .25 })
 		
 		.addLabel('scroll-finished')
 	}
